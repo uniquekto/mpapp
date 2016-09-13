121d4284d2513bc0ee62ed293bcfe93a85ff90d5
Pramesh <pramesh_morang@hotmail.com>
2016-09-13 16:07:33 +0300

    app.js modified

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('port', (process.env.PORT || 80));
+
+app.get('/', function(req, res) {
+
+res.send("Hello world");
+
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+
+console.log('Node app is running on port', app.get('port'));
+
 });
